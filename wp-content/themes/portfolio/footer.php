95b3c317b3205315421241a2ceff21ba3a8dbc86
benjamin <benjamin.marissiaux@student.hepl.be>
2024-07-27 12:06:35 +0200

    - about page finished - details on other pages corrected

--- wp-content/themes/portfolio/footer.php
+++ wp-content/themes/portfolio/footer.php
@@ -1,77 +1,78 @@
 <footer class="footer">
-    <h2 class="hidden-title">footer</h2>
-    <section class="footer__section">
-        <h3 class="footer__section__titre">Coordonnées</h3>
-        <ul class="footer__section__list">
-            <li class="footer__section__list__item">
-                tel&nbsp;:&nbsp;<a href="tel:+32474405122" title="Téléphoner à Benjamin Marissiaux"
-                                   class="footer__section__list__item__link">+32&nbsp;474&nbsp;40&nbsp;51&nbsp;22</a>
-            </li>
-            <li class="footer__section__list__item">
-                mail&nbsp;:&nbsp;<a href="mailto:ben.marissiaux@gmail.com" class="footer__section__list__item__mail">ben.marissiaux@gmail.com</a>
-            </li>
-            <li class="footer__section__list__item">
-                Adresse&nbsp;:&nbsp;Grand&lsquo;Route 342,
-                <br>4347 Fexhe-le-haut-Clocher
-            </li>
-        </ul>
-    </section>
-    <section class="footer__section">
-        <h3 class="footer__section__titre">Navigation</h3>
-        <ul class="footer__section__list">
-            <li class="footer__section__list__item">
-                <a href="http://portfolio-v2.test/accueil/" title="Aller vers la page d'accueil"
-                   class="footer__section__list__item__nav">
-                    Accueil
-                </a>
-            </li>
-            <li class="footer__section__list__item">
-                <a href="http://portfolio-v2.test/A propos/" title="Aller vers la page a propos"
-                   class="footer__section__list__item__nav">
-                    A propos
-                </a>
-            </li>
-            <li class="footer__section__list__item">
-                <a href="http://portfolio-v2.test/Projets/" title="Aller vers la page projets"
-                   class="footer__section__list__item__nav">
-                    Projets
-                </a>
-            </li>
-            <li class="footer__section__list__item">
-                <a href=" http://portfolio-v2.test/contact/" title="Aller vers la page contact"
-                   class="footer__section__list__item__nav">
-                    Contact
-                </a>
-            </li>
-        </ul>
-    </section>
-    <section class="footer__section">
-        <h3 class="footer__section__titre">Réseaux</h3>
-        <ul class="footer__section__socials-list">
-            <li class="footer__section__socials-list__item">
-                <a href="https://www.instagram.com/ben.m.0_o/" title="Aller sur l'instagram de Benjamin Marissiaux"
-                   class="footer__section__socials-list__item__insta-link"
-                   target="_blank">
-                    insta
-                </a>
-            </li>
-            <li class="footer__section__socials-list__item">
-                <a href="https://github.com" title="Aller sur le github de Benjamin Marissiaux"
-                   class="footer__section__socials-list__item__github-link"
-                   target="_blank">
-                    github
-                </a>
-            </li>
-            <li class="footer__section__socials-list__item">
-                <a href="https://www.linkedin.com/in/benjamin-marissiaux-2aa7bb309/"
-                   title="Aller sur le linkedin de Benjamin Marissiaux"
-                   class="footer__section__socials-list__item__linkedin-link"
-                   target="_blank">
-                    linkedin
-                </a>
-            </li>
-        </ul>
-    </section>
+        <h2 class="hidden-title">footer</h2>
+        <section class="footer__section">
+            <h3 class="footer__section__titre">Coordonnées</h3>
+            <ul class="footer__section__list">
+                <li class="footer__section__list__item">
+                    tel&nbsp;:&nbsp;<a href="tel:+32474405122" title="Téléphoner à Benjamin Marissiaux"
+                                       class="footer__section__list__item__link">+32&nbsp;474&nbsp;40&nbsp;51&nbsp;22</a>
+                </li>
+                <li class="footer__section__list__item">
+                    mail&nbsp;:&nbsp;<a href="mailto:ben.marissiaux@gmail.com"
+                                        class="footer__section__list__item__mail">ben.marissiaux@gmail.com</a>
+                </li>
+                <li class="footer__section__list__item">
+                    Adresse&nbsp;:&nbsp;Grand&lsquo;Route 342,
+                    <br>4347 Fexhe-le-haut-Clocher
+                </li>
+            </ul>
+        </section>
+        <section class="footer__section">
+            <h3 class="footer__section__titre">Navigation</h3>
+            <ul class="footer__section__list">
+                <li class="footer__section__list__item">
+                    <a href="http://portfolio-v2.test/accueil/" title="Aller vers la page d'accueil"
+                       class="footer__section__list__item__nav">
+                        Accueil
+                    </a>
+                </li>
+                <li class="footer__section__list__item">
+                    <a href="http://portfolio-v2.test/A propos/" title="Aller vers la page a propos"
+                       class="footer__section__list__item__nav">
+                        A propos
+                    </a>
+                </li>
+                <li class="footer__section__list__item">
+                    <a href="http://portfolio-v2.test/Projets/" title="Aller vers la page projets"
+                       class="footer__section__list__item__nav">
+                        Projets
+                    </a>
+                </li>
+                <li class="footer__section__list__item">
+                    <a href=" http://portfolio-v2.test/contact/" title="Aller vers la page contact"
+                       class="footer__section__list__item__nav">
+                        Contact
+                    </a>
+                </li>
+            </ul>
+        </section>
+        <section class="footer__section">
+            <h3 class="footer__section__titre">Réseaux</h3>
+            <ul class="footer__section__socials-list">
+                <li class="footer__section__socials-list__item">
+                    <a href="https://www.instagram.com/ben.m.0_o/" title="Aller sur l'instagram de Benjamin Marissiaux"
+                       class="footer__section__socials-list__item__insta-link"
+                       target="_blank">
+                        insta
+                    </a>
+                </li>
+                <li class="footer__section__socials-list__item">
+                    <a href="https://github.com" title="Aller sur le github de Benjamin Marissiaux"
+                       class="footer__section__socials-list__item__github-link"
+                       target="_blank">
+                        github
+                    </a>
+                </li>
+                <li class="footer__section__socials-list__item">
+                    <a href="https://www.linkedin.com/in/benjamin-marissiaux-2aa7bb309/"
+                       title="Aller sur le linkedin de Benjamin Marissiaux"
+                       class="footer__section__socials-list__item__linkedin-link"
+                       target="_blank">
+                        linkedin
+                    </a>
+                </li>
+            </ul>
+        </section>
 </footer>
 <div class="legal-notice-container">
     <p class="legale-notice-container__credits">

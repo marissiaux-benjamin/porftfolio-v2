7b3073c37e133ac3f5487989c68b553428304620
benjamin <benjamin.marissiaux@student.hepl.be>
2024-07-27 15:25:46 +0200

    - footer mediaquery fixed - legal notices finished

---
+++ wp-content/themes/portfolio/mentions-legales.php
@@ -0,0 +1,114 @@
+<?php
+
+/*
+Template Name: mentions
+*/
+
+get_header();
+?>
+<main class="main-dark-background">
+    <div class="background">
+        <h1 class="landing-titre">
+            Mentions l&eacute;gales
+        </h1>
+
+        <section class="mention coordonnees">
+            <div class="wrapper">
+                <h1 class="mention__titre ">
+                    Coordonnées&nbsp;:
+                </h1>
+                <p class="mention__texte">
+                    <span>Adresse&nbsp;:</span> Grand'Route 342, 4347 Fexhe-le-Haut-Clocher Belgique
+                </p>
+                <p class="mention__texte">
+                    <span> Tel&nbsp;:</span> +32 474 40 51 22
+                </p>
+
+                <p class="mention__texte">
+                    <span>E-mail&nbsp;:</span> ben.marissiaux@gmail.com
+                </p>
+
+                <p class="mention__texte">
+                    <span>Responsable de la publication&nbsp;:</span> Benjamin Marissiaux
+                </p>
+
+            </div>
+        </section>
+
+        <section class="mention">
+            <div class="wrapper">
+                <h1 class="mention__titre">
+                    Propriété intellectuelle&nbsp;:
+                </h1>
+                <p class="mention__texte">
+                    Tous les contenus présents sur ce site, y compris les textes, les images, les vidéos et les éléments
+                    graphiques, sont la propriété exclusive de Benjamin Marissiaux, sauf indication contraire. Toute
+                    reproduction, distribution, modification ou utilisation non autorisée de ces contenus est
+                    strictement interdite.
+                </p>
+            </div>
+        </section>
+        <section class="mention">
+            <div class="wrapper">
+                <h1 class="mention__titre">
+                    Liens externes&nbsp;:
+                </h1>
+                <p class="mention__texte">
+                    Notre site peut contenir des liens vers des sites web externes qui sont fournis uniquement à titre
+                    informatif. Nous n’avons aucun contrôle sur le contenu de ces sites et déclinons toute
+                    responsabilité quant à leur exactitude, leur légalité ou leur fiabilité.
+                </p>
+            </div>
+        </section>
+        <section class="mention">
+            <div class="wrapper">
+                <h1 class="mention__titre">
+                    Collecte de données personnelles&nbsp;:
+                </h1>
+                <p class="mention__texte">
+                    Nous collectons des données personnelles uniquement lorsque vous les fournissez volontairement, par
+                    exemple en remplissant un formulaire de contact Les informations collectées sont utilisées dans le
+                    cadre des services que vous avez demandés et ne seront pas partagées avec des tiers sans votre
+                    consentement préalable.
+                </p>
+            </div>
+        </section>
+        <section class="mention">
+            <div class="wrapper">
+                <h1 class="mention__titre">
+                    Sécurité des données&nbsp;:
+                </h1>
+                <p class="mention__texte">
+                    Nous mettons en place des mesures de sécurité appropriées pour protéger vos données personnelles
+                    contre tout accès non autorisé, perte ou divulgation. Cependant, veuillez noter qu’aucune méthode de
+                    transmission sur Internet ou de stockage électronique n’est totalement sécurisée.
+                </p>
+            </div>
+        </section>
+        <section class="mention">
+            <div class="wrapper">
+                <h1 class="mention__titre">
+                    Droits d'accès, de rectification et de suppression&nbsp;:
+                </h1>
+                <p class="mention__texte">
+                    Vous avez le droit d’accéder aux données personnelles que nous détenons vous concernant et de les
+                    mettre à jour ou de les supprimer si nécessaire. Pour exercer ces droits, veuillez nous contacter à
+                    l’adresse e-mail indiquée ci-dessus.
+                </p>
+            </div>
+        </section>
+        <section class="mention">
+            <div class="wrapper">
+                <h1 class="mention__titre">
+                    Modifications des mentions légales&nbsp;:
+                </h1>
+                <p class="mention__texte">
+                    Nous nous réservons le droit de modifier ces mentions légales à tout moment. Les modifications
+                    seront publiées sur cette page, et la date de la dernière mise à jour sera indiquée en haut de la
+                    page.
+                </p>
+            </div>
+        </section>
+    </div>
+</main>
+<?php get_footer() ?>

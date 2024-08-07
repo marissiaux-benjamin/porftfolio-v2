d2c04d355be332695a89b37f30c526e2c10ca1d5
benjamin <benjamin.marissiaux@student.hepl.be>
2024-08-07 18:33:00 +0200

    - contact page form done

--- wp-content/themes/portfolio/contact.php
+++ wp-content/themes/portfolio/contact.php
@@ -11,26 +11,34 @@ get_header()
 <main class="main-dark-background">
     <section class="landing">
         <div class="wrapper">
-            <h1 class="landing__titre">Contactez&nbsp;-&nbsp;moi</h1>
-            <p class="landing__description">
-                Vous voulez prendre contact avec moi&nbsp;?
+            <h1 class="landing__titre">
+                Contactez&nbsp;&nbsp;moi
+            </h1>
+            <p class="landing__texte">
+                Êtes&nbsp;-&nbsp;vous int&eacute;r&eacute;ss&eacute; par mes services&nbsp;? Contactez moi grâce au
+                formulaire ci&nbsp;-&nbsp;dessous.
             </p>
-            <div class="landing__btn-container">
-                <a href="<?= get_field('button-about', 10); ?>" title="Aller vers la page a propos" class="btn">
-                    Me découvrir
-                </a>
-            </div>
-            <div class="landing__arrow-container">
-                <p>
-                    <?= get_field('text') ?>
-                </p>
-                <img src="http://portfolio-v2.test/wp-content/uploads/2024/07/fleche.svg"
-                     alt="fleche pointant vers le bas"
-                     class="landing__arrow-container__arrow">
-            </div>
         </div>
     </section>
 
+    <section class="formulaire">
+        <div class="wrapper">
+            <form action="#" method="get">
+                <label for="nom">Nom et pr&eacute;nom</label>
+                <input type="text" id="nom" class="input" placeholder="exemple&nbsp;: Charles Palu">
 
+                <label for="mail">Nom et pr&eacute;nom</label>
+                <input type="text" id="mail" class="input" placeholder="exemple&nbsp;: charle@gmail.com">
 
+                <label for="message">Votre message</label>
+                <textarea name="message" id="message" cols="30" rows="10" class="input"
+                          placeholder="Bonjour, j'ai vu votre travail et j'aimerais vous contacter pour..."></textarea>
+
+                <input type="submit" value="Envoyer" class="">
+            </form>
+        </div>
+    </section>
 </main>
+
+
+

37bad21701c93b3ab2f41297d57b9aa793ade3a7
benjamin <benjamin.marissiaux@student.hepl.be>
2024-08-08 14:24:48 +0200

    - contact page form finished

--- wp-content/themes/portfolio/contact.php
+++ wp-content/themes/portfolio/contact.php
@@ -12,25 +12,29 @@ get_header()
     <section class="landing">
         <div class="wrapper">
             <h1 class="landing__titre">
-                Contactez&nbsp;&nbsp;moi
+                Contactez&nbsp;-&nbsp;moi&nbsp;!
             </h1>
             <p class="landing__texte">
                 Êtes&nbsp;-&nbsp;vous int&eacute;r&eacute;ss&eacute; par mes services&nbsp;? Contactez moi grâce au
                 formulaire ci&nbsp;-&nbsp;dessous.
+            </p>
+            <p>
+                Attention, tous les champs sont obligatoires.
             </p>
         </div>
     </section>
 
     <section class="formulaire">
         <div class="wrapper">
+
             <form action="#" method="get">
-                <label for="nom">Nom et pr&eacute;nom</label>
+                <label for="nom">Nom et pr&eacute;nom *</label>
                 <input type="text" id="nom" class="input" placeholder="exemple&nbsp;: Charles Palu">
 
-                <label for="mail">Nom et pr&eacute;nom</label>
+                <label for="mail">Votre mail *</label>
                 <input type="text" id="mail" class="input" placeholder="exemple&nbsp;: charle@gmail.com">
 
-                <label for="message">Votre message</label>
+                <label for="message">Votre message *</label>
                 <textarea name="message" id="message" cols="30" rows="10" class="input"
                           placeholder="Bonjour, j'ai vu votre travail et j'aimerais vous contacter pour..."></textarea>
 
@@ -39,6 +43,8 @@ get_header()
         </div>
     </section>
 </main>
+
+<?= get_footer() ?>
 
 
 
